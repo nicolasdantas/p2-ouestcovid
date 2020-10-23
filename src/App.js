9ae0830a9f8fc9403886c85e42b1c913d007826b
nicolasdantas <nicolas.dantas@gmail.com>
2020-10-23 11:24:01 +0200

    modify routes (create components Navbar and Footer)

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import WhereToGo from './components/WhereToGo';
 import Home from './components/Home';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <Router>
-      <header>
-        <h1>I'm the header</h1>
-        <Link to='/'>Accueil</Link>
-        <Link to='/ou-partir'>Où partir ?</Link>
-      </header>
+      <Navbar />
       <Switch>
         <Route exact path='/' component={Home} />
         <Route exact path='/ou-partir' component={WhereToGo} />
       </Switch>
-      <footer>
-        <h1>I'm the footer</h1>
-      </footer>
+      <Footer />
     </Router>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+  return (
+    <header>
+      <h1>I'm the header</h1>
+      <Link to='/'>Accueil </Link>
+      <Link to='/ou-partir'>Où partir ?</Link>
+    </header>
+  );
+};
+
+export default Navbar;
