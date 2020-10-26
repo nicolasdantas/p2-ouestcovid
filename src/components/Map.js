f17d465650feef00dc700e602b28fcba2a0d2dcc
nicolasdantas <nicolas.dantas@gmail.com>
2020-10-26 20:07:20 +0100

    add a console.log to display county clicked

--- src/components/Map.js
+++ src/components/Map.js
@@ -6,10 +6,22 @@ import './Map.scss';
 class Map extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      clickedLocationName: null,
+      clickedLocationId: null,
+    };
   }
 
+  handleClick = (event) => {
+    this.setState({
+      clickedLocationName: event.target.attributes.name.value,
+      clickedLocationId: event.target.id,
+    });
+    console.log(this.state.clickedLocationName, this.state.clickedLocationId);
+  };
+
   render() {
-    return <SVGMap map={France} />;
+    return <SVGMap map={France} onLocationClick={this.handleClick} />;
   }
 }
 
