e36c28cddc855bff868c3693f1ad9883ba9e6072
ThomasChevaleraud <71005456+ThomasChevaleraud@users.noreply.github.com>
2020-10-27 15:49:23 +0100

    ébauche du footer 1

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,48 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer>
-      <h1>I'm the footer</h1>
+      <div>
+        <ul>
+          <li>
+            <a href="https://www.facebook.com">
+              <img
+                src="https://www.bing.com/th?id=AMMS_8ddf76a14a2e3ad3ba62b46d49a75a74&w=110&h=110&c=7&rs=1&qlt=95&cdv=1&pid=16.1"
+                alt="logo Facebook"
+              />
+            </a>
+          </li>
+          <li>
+            <a href="https://www.twitter.com">
+              <img
+                src="https://www.bing.com/th?id=AMMS_c4fa0d273f63fef684d0e182a47bc002&w=110&h=110&c=7&rs=1&qlt=95&cdv=1&pid=16.1"
+                alt="logo Twitter"
+              />
+            </a>
+          </li>
+          <li>
+            <a href="https://www.gitub.com">
+              <img
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Octicons-mark-github.svg/330px-Octicons-mark-github.svg.png"
+                alt="logo Github"
+              />
+            </a>
+          </li>
+          <li>
+            <a href="https://www.gouvernement.fr/info-coronavirus">
+              <img
+                src="https://www.bing.com/th?id=AMMS_6bac0145b70a382c723ea52e1eb6170b&w=110&h=110&c=7&rs=1&qlt=95&cdv=1&pid=16.1"
+                alt="logo du gouvernement Français"
+              />
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <p>
+          Made with madness by Matt Damon, Guiness Man, Moufette Anonyme, Lou
+          Lou, Moi Même
+        </p>
+      </div>
     </footer>
   );
 };
