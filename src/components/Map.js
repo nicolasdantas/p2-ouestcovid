62c5e375cd8dd2cde947f8169d2e983a9bd8eb54
nicolasdantas <nicolas.dantas@gmail.com>
2020-10-27 16:53:32 +0100

    request API by clicking the map working

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,27 +1,60 @@
 import React from 'react';
 import France from '@svg-maps/france.departments';
 import { SVGMap } from 'react-svg-map';
+import axios from 'axios';
 import './Map.scss';
 
 class Map extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      clickedLocationName: null,
-      clickedLocationId: null,
+      clickedLocationName: 'aucun',
+      clickedLocationId: '',
+      dataCounty: [],
+      dataCountyLoaded: false,
     };
   }
 
   handleClick = (event) => {
     this.setState({
       clickedLocationName: event.target.attributes.name.value,
-      clickedLocationId: event.target.id,
     });
-    console.log(this.state.clickedLocationName, this.state.clickedLocationId);
+
+    axios
+      .get(
+        event.target.attributes.name.value === 'Ville de Paris'
+          ? `https://coronavirusapi-france.now.sh/LiveDataByDepartement?Departement=Paris`
+          : `https://coronavirusapi-france.now.sh/LiveDataByDepartement?Departement=${event.target.attributes.name.value}`
+      )
+      .then((response) => response.data)
+      .then((data) => {
+        this.setState({
+          dataCounty: data.LiveDataByDepartement[0],
+          dataCountyLoaded: true,
+        });
+      });
   };
 
   render() {
-    return <SVGMap map={France} onLocationClick={this.handleClick} />;
+    const { dataCounty } = this.state;
+    console.log(dataCounty);
+    return (
+      <div>
+        <SVGMap map={France} onLocationClick={this.handleClick} />
+        {this.state.dataCountyLoaded ? (
+          <div>
+            <p>
+              Situation sanitaire dans le {dataCounty.nom} au {dataCounty.date}
+            </p>
+            <p>Hospitalisés : {dataCounty.hospitalises}</p>
+            <p>Décès : {dataCounty.deces}</p>
+            <p>Guéris : {dataCounty.gueris}</p>
+          </div>
+        ) : (
+          <p>false</p>
+        )}
+      </div>
+    );
   }
 }
 
