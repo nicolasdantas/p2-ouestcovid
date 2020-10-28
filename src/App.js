e558c81a97eced0abb1c0c11c80748a7f2fd8097
louisececcaldi <comacle.louise@gmail.com>
2020-10-28 09:26:15 +0100

    Mise en place du menu hamburger et du logo

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import WhereToGo from './components/WhereToGo';
 import Home from './components/Home';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import './App.css';
+
 
 function App() {
   return (
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Navbar.css';
+
 
 const Navbar = () => {
   return (
     <header>
-      <h1>I'm the header</h1>
-      <Link to='/'>Accueil </Link>
-      <Link to='/ou-partir'>Où partir ?</Link>
+      <nav className="menu" role="navigation">
+        <div className="inner">  
+          <div className = "navBarLeft">
+          <img src={require("../pictures/logo.png")} alt="logo"/>
+          </div>
+          
+          <div className="navBarRight">
+          <button id="hamburger_button">&#9776;</button>
+            <ul>
+            <li className="liste"><Link to='/'>Accueil </Link></li>
+            <li className="liste"><Link to='/ou-partir'>Où partir ?</Link></li>
+            </ul>
+          </div> 
+        </div>     
+      </nav>
     </header>
   );
 };
