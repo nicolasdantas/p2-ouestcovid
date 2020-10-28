e6bfcab2be4d14751f1ce23e3e53f1fe37560a90
ThomasChevaleraud <71005456+ThomasChevaleraud@users.noreply.github.com>
2020-10-28 09:24:36 +0100

    rectification des logos des liens de redirection

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,12 +3,12 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer>
-      <div>
+      <div className="divLinkFooter">
         <ul>
           <li>
             <a href="https://www.facebook.com">
               <img
-                src="https://www.bing.com/th?id=AMMS_8ddf76a14a2e3ad3ba62b46d49a75a74&w=110&h=110&c=7&rs=1&qlt=95&cdv=1&pid=16.1"
+                src={require('../img/facebook-logo.png')}
                 alt="logo Facebook"
               />
             </a>
@@ -16,33 +16,30 @@ const Footer = () => {
           <li>
             <a href="https://www.twitter.com">
               <img
-                src="https://www.bing.com/th?id=AMMS_c4fa0d273f63fef684d0e182a47bc002&w=110&h=110&c=7&rs=1&qlt=95&cdv=1&pid=16.1"
+                src={require('../img/twitter-logo.png')}
                 alt="logo Twitter"
               />
             </a>
           </li>
           <li>
             <a href="https://www.gitub.com">
-              <img
-                src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Octicons-mark-github.svg/330px-Octicons-mark-github.svg.png"
-                alt="logo Github"
-              />
+              <img src={require('../img/github-logo.png')} alt="logo Github" />
             </a>
           </li>
           <li>
             <a href="https://www.gouvernement.fr/info-coronavirus">
               <img
-                src="https://www.bing.com/th?id=AMMS_6bac0145b70a382c723ea52e1eb6170b&w=110&h=110&c=7&rs=1&qlt=95&cdv=1&pid=16.1"
-                alt="logo du gouvernement Français"
+                src={require('../img/gouvernement-logo.png')}
+                alt="logo du gouvernement de la république Française"
               />
             </a>
           </li>
         </ul>
       </div>
-      <div>
+      <div className="divTag">
         <p>
-          Made with madness by Matt Damon, Guiness Man, Moufette Anonyme, Lou
-          Lou, Moi Même
+          Made with madness by Matt Damon, Candide Guiness , Moufette Anonyme,
+          Lou Lou, Moi Même
         </p>
       </div>
     </footer>
