bf08016a2811e9378bcec244d4be43f83d3fdfec
nicolasdantas <nicolas.dantas@gmail.com>
2020-10-28 14:41:08 +0100

    news carousel works with news from google news

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -25,71 +25,15 @@ class News extends React.Component {
   };
 
   render() {
-    console.log(this.state.arrayOfNews);
+    const { arrayOfNews } = this.state;
     return (
       <Carousel className="carousel" autoPlay showThumbs={false}>
-        {this.state.arrayOfNews.map((article) => (
-          <div>
-            <img alt="" src={article.urlToImage}></img>
+        {arrayOfNews.map((article) => (
+          <div key={article.title}>
+            <img alt="" src={article.urlToImage} />
             <p className="legend">{article.title}</p>
           </div>
         ))}
-        {/* <div>
-          <img alt="" src="http://lorempixel.com/output/cats-q-c-640-480-1.jpg" />
-          <p className="legend">Legend 1</p>
-        </div>
-        <div>
-          <img alt="" src="http://lorempixel.com/output/cats-q-c-640-480-2.jpg" />
-          <p className="legend">Legend 2</p>
-        </div>
-        <div>
-          <img alt="" src="http://lorempixel.com/output/cats-q-c-640-480-3.jpg" />
-          <p className="legend">Legend 3</p>
-        </div>
-        <div>
-          <img alt="" src="http://lorempixel.com/output/cats-q-c-640-480-4.jpg" />
-          <p className="legend">Legend 4</p>
-        </div>
-        <div>
-          <img alt="" src="http://lorempixel.com/output/cats-q-c-640-480-5.jpg" />
-          <p className="legend">Legend 5</p>
-        </div>
-        <div>
-          <img alt="" src="http://lorempixel.com/output/cats-q-c-640-480-6.jpg" />
-          <p className="legend">Legend 6</p>
-        </div>
-        <div>
-          <img alt="" src="http://lorempixel.com/output/cats-q-c-640-480-7.jpg" />
-          <p className="legend">Legend 7</p>
-        </div>
-        <div>
-          <img alt="" src="http://lorempixel.com/output/cats-q-c-640-480-8.jpg" />
-          <p className="legend">Legend 8</p>
-        </div>
-        <div>
-          <img alt="" src="http://lorempixel.com/output/cats-q-c-640-480-9.jpg" />
-          <p className="legend">Legend 9</p>
-        </div>
-        <div>
-          <img alt="" src="http://lorempixel.com/output/cats-q-c-640-480-10.jpg" />
-          <p className="legend">Legend 10</p>
-        </div>
-        <div>
-          <img alt="" src="http://lorempixel.com/output/cats-q-c-640-480-11.jpg" />
-          <p className="legend">Legend 11</p>
-        </div>
-        <div>
-          <img alt="" src="http://lorempixel.com/output/cats-q-c-640-480-12.jpg" />
-          <p className="legend">Legend 12</p>
-        </div>
-        <div>
-          <img alt="" src="http://lorempixel.com/output/cats-q-c-640-480-13.jpg" />
-          <p className="legend">Legend 13</p>
-        </div>
-        <div>
-          <img alt="" src="http://lorempixel.com/output/cats-q-c-640-480-14.jpg" />
-          <p className="legend">Legend 14</p>
-        </div> */}
       </Carousel>
     );
   }
