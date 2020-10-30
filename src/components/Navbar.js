e7763d33dab15eb924e701114dfe2770ef2efcf2
louisececcaldi <comacle.louise@gmail.com>
2020-10-30 09:22:31 +0100

    Ajout de l'extension scss dans le navbar.css, ajout du lien sur le logo, ajout des fontsize par default dans le Global.css

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,30 +1,56 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './Navbar.css';
+import './Navbar.scss';
 import CustomizedMenus from './Burger';
 
 const Navbar = () => {
   return (
-    <header>
-      <nav className="menu" role="navigation">
-        <div className="inner">  
-          <div className = "navBarLeft">
-          <img className="logo" src={require("../pictures/logo.png")} alt="logo" width="70px" height="70px" />
+    <header className='header'>
+      <nav className='menu' role='navigation'>
+        <div className='inner'>
+          <div className='navBarLeft'>
+            <Link to='/'>
+              <img
+                className='logo'
+                src={require('../pictures/logo.png')}
+                alt='logo'
+                width='70px'
+                height='70px'
+              />
+            </Link>
           </div>
-          
-          <div className="navBarRight">
-          <div><CustomizedMenus/></div>
-            <ul class="lien-navbar">
-            <li><Link className="liste" to='/'>Statistiques </Link></li>
-            <li><Link className="liste" to='/ou-partir'>Où partir ?</Link></li>
-            <li><a className="liste" href='#news'>News</a></li>
-            <li><a className="liste" href='#contact'>Contact</a></li>
+
+          <div className='navBarRight'>
+            <div>
+              <CustomizedMenus />
+            </div>
+            <ul class='lien-navbar'>
+              <li>
+                <Link className='liste' to='/'>
+                  Statistiques{' '}
+                </Link>
+              </li>
+              <li>
+                <Link className='liste' to='/ou-partir'>
+                  Où partir ?
+                </Link>
+              </li>
+              <li>
+                <a className='liste' href='#news'>
+                  News
+                </a>
+              </li>
+              <li>
+                <a className='liste' href='#contact'>
+                  Contact
+                </a>
+              </li>
             </ul>
-          </div>   
-        </div>     
+          </div>
+        </div>
       </nav>
     </header>
-  ); 
+  );
 };
 
 export default Navbar;
