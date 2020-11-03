d47db017af6ccd0f086f6dc759923d0ca070e32a
nicolasdantas <nicolas.dantas@gmail.com>
2020-11-03 20:43:05 +0100

    react-select working

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,31 +1,25 @@
 import React from 'react';
-import { Autocomplete } from '@material-ui/lab';
+import Select from 'react-select';
 import countyList from './countyList.json'; // data from https://geo.api.gouv.fr/departements
 
-class SearchBar extends React.Component {
-  handleCountySelection = (newValue) => {
+const SearchBar = (props) => {
+  const handleCountySelection = (newValue) => {
     const countyName = newValue;
+    console.log(newValue);
     if (countyName) {
       const countyCode = countyList.find((county) => county.nom === countyName);
-      const { onSelectCounty } = this.propqs;
+      const { onSelectCounty } = props;
       onSelectCounty(countyCode.code);
     }
   };
 
-  render() {
-    return (
-      <Autocomplete
-        onChange={(event, newValue) => this.handleCountySelection(newValue)}
-        id="custom-input-demo"
-        options={countyList.map((county) => county.nom)}
-        renderInput={(params) => (
-          <div ref={params.InputProps.ref}>
-            <input style={{ width: 200 }} type="text" {...params.inputProps} />
-          </div>
-        )}
-      />
-    );
-  }
-}
+  return (
+    <Select
+      options={countyList.map((county) => county.nom)}
+      getOptionLabel={(option) => `${option}`}
+      onChange={(newValue) => handleCountySelection(newValue)}
+    />
+  );
+};
 
 export default SearchBar;
