007ed1301f513150079a30e3b50334705f9e4aac
nicolasdantas <nicolas.dantas@gmail.com>
2020-11-05 10:43:59 +0100

    started changing colors

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,46 +8,38 @@ const Navbar = () => {
   return (
     <header className="header">
       <nav className="menu" role="navigation">
-        <div className="inner">
-          <div className="navbar-left">
-            <Link to="/">
-              <img
-                className="logo"
-                src={logo}
-                alt="logo"
-                width="70px"
-                height="70px"
-              />
-            </Link>
-          </div>
+        <div className="navbar-left">
+          <Link to="/">
+            <img className="logo" src={logo} alt="logo" />
+          </Link>
+        </div>
 
-          <div className="navbar-right">
-            <div>
-              <CustomizedMenus />
-            </div>
-            <ul className="lien-navbar">
-              <li>
-                <Link className="liste" to="/">
-                  Statistiques
-                </Link>
-              </li>
-              <li>
-                <Link className="liste" to="/ou-partir">
-                  Où partir ?
-                </Link>
-              </li>
-              <li>
-                <a className="liste" href="#news">
-                  News
-                </a>
-              </li>
-              <li>
-                <a className="liste" href="#contact">
-                  Contact
-                </a>
-              </li>
-            </ul>
+        <div className="navbar-right">
+          <div>
+            <CustomizedMenus />
           </div>
+          <ul className="lien-navbar">
+            <li>
+              <Link className="liste" to="/">
+                Statistiques
+              </Link>
+            </li>
+            <li>
+              <Link className="liste" to="/ou-partir">
+                Où partir ?
+              </Link>
+            </li>
+            <li>
+              <a className="liste" href="#news">
+                News
+              </a>
+            </li>
+            <li>
+              <a className="liste" href="#contact">
+                Contact
+              </a>
+            </li>
+          </ul>
         </div>
       </nav>
     </header>
