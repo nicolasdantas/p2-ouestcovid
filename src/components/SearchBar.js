2dd2e73f7c9b5ca33ad9e78b21965454c22c2f6d
nicolasdantas <nicolas.dantas@gmail.com>
2020-11-05 09:52:53 +0100

    fixed bug when choosing a county by the list

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -5,19 +5,15 @@ import countyList from './countyList.json'; // data from https://geo.api.gouv.fr
 const SearchBar = (props) => {
   const [selectedCounty, setSelectedCounty] = useState('');
 
-  useEffect(
-    // eslint-disable-next-line no-shadow
-    (props) => {
-      if (selectedCounty) {
-        const countyCode = countyList.find(
-          (county) => county.nom === selectedCounty
-        );
-        const { onSelectCounty } = props;
-        onSelectCounty(countyCode.code);
-      }
-    },
-    [selectedCounty]
-  );
+  useEffect(() => {
+    if (selectedCounty) {
+      const countyCode = countyList.find(
+        (county) => county.nom === selectedCounty
+      );
+      const { onSelectCounty } = props;
+      onSelectCounty(countyCode.code);
+    }
+  }, [selectedCounty]);
 
   const customStyles = {
     control: (provided) => ({
