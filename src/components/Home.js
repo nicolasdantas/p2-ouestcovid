32ec7599c1878d768b608a20754524d4ab49a2df
nicolasdantas <nicolas.dantas@gmail.com>
2020-11-09 19:00:43 +0100

    banner done

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ContactForm from './ContactForm';
 import News from './News';
 import DataByCounty from './DataByCounty';
+import Banner from './Banner';
 
 const Home = () => {
   return (
     <div>
+      <Banner />
       <DataByCounty />
       <News />
       <ContactForm />
