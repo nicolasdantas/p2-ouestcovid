df4149de079b6de4c6ca32a9023859b89b81fe66
AlexButeau <alexbuteau1@gmail.com>
2020-11-10 17:47:26 +0100

    first working version with a checkbox and colors for reanimation

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,10 +1,81 @@
 import React, { useState, useEffect } from 'react';
 import France from '@svg-maps/france.departments';
 import { SVGMap } from 'react-svg-map';
+import axios from 'axios';
+import moment from 'moment';
 import './style/Map.scss';
 import { TransformWrapper, TransformComponent } from 'react-zoom-pan-pinch';
+//import { red } from '@material-ui/core/colors';
 
 const Map = (props) => {
+  const [allData, setData] = React.useState([]);
+  const [checkboxRea, setCheckboxRea] = React.useState(false);
+  const dayMinus1 = moment().subtract(1, 'days').format('YYYY-MM-DD'); // last available data
+
+  React.useEffect(() => {
+    const { CancelToken } = axios;
+    const source = CancelToken.source();
+    axios
+      .get(
+        `https://coronavirusapi-france.now.sh/AllDataByDate?date=${dayMinus1}`,
+        {
+          cancelToken: source.token,
+        }
+      )
+      .then((response) => response.data)
+      .then((data) => {
+        setData(() =>
+          /* getting the data from API, comparing it to countyList.json based on county code and adding for each county the number of beds in reanimation */
+          data.allFranceDataByDate.filter((item) => item.code.includes('DEP'))
+        );
+      })
+      .catch((err) => {
+        if (axios.isCancel(err)) {
+          console.log('Request canceled', err.message);
+        }
+      }); // eslint-disable-next-line
+    return function cleanup() {
+      // cancels the previous request on unmount or query update :
+      source.cancel('Operation canceled by the user.');
+    }; // eslint-disable-next-line
+  }, []);
+
+  const customFrance =
+    allData.length > 0 && checkboxRea === true //on vérifie qu'on a reçu les données de l'API, et que la box est check (celle "afficher un gradient de couleur" etc)
+      ? {
+          ...France,
+          label: 'Custom map label',
+          locations: France.locations.map((location) => {
+            // Modify each location
+            let nbRea = allData.find(
+              (item) => item.code.split('-')[1] === location.id
+            ).reanimation;
+
+            if (nbRea > 250) {
+              return {
+                ...location,
+                name: location.name + '-red',
+              };
+            } else if (nbRea > 100) {
+              return {
+                ...location,
+                name: location.name + '-orange',
+              };
+            } else if (nbRea > 50) {
+              return {
+                ...location,
+                name: location.name + '-yellow',
+              };
+            } else {
+              return {
+                ...location,
+                name: location.name + '-white',
+              };
+            }
+          }),
+        }
+      : France;
+
   const [windowWidth, setWindowWidth] = useState(window.innerWidth);
 
   const handleResize = () => {
@@ -21,14 +92,26 @@ const Map = (props) => {
 
   return (
     <div className="map none">
+      <div>
+        <input
+          type="checkbox"
+          id="sort-color"
+          name="sort-color"
+          onChange={() => setCheckboxRea((prevState) => !prevState)}
+          checked={checkboxRea}
+        />
+        <label htmlFor="sort-color">
+          Afficher un gradient de couleur selon le nombre de personnes en réa
+        </label>
+      </div>
       {windowWidth < 600 ? (
         <TransformWrapper>
           <TransformComponent>
-            <SVGMap map={France} onLocationClick={handleClick} />
+            <SVGMap map={customFrance} onLocationClick={handleClick} />
           </TransformComponent>
         </TransformWrapper>
       ) : (
-        <SVGMap map={France} onLocationClick={handleClick} />
+        <SVGMap map={customFrance} onLocationClick={handleClick} />
       )}
     </div>
   );
