76b92fa860b291e4279ab7d857ba7fe4cdf12380
nicolasdantas <nicolas.dantas@gmail.com>
2020-11-15 15:26:17 +0100

    Global.css to Global.scss

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './components/style/Global.css';
+import './components/style/Global.scss';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import WhereToGo from './components/WhereToGo';
 import Home from './components/Home';
