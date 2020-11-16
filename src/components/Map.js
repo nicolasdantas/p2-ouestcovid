eeeaf47087b901016db517690ba4d5e2e9be760e
nicolasdantas <nicolas.dantas@gmail.com>
2020-11-16 17:39:37 +0100

    Map refacto

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,11 +1,12 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import France from '@svg-maps/france.departments';
 import { SVGMap } from 'react-svg-map';
-import axios from 'axios';
-import moment from 'moment';
 import Select from 'react-select';
 import './style/Map.scss';
 import { TransformWrapper, TransformComponent } from 'react-zoom-pan-pinch';
+import countyListPop from './datas/countyListPop.json';
+import { CountySelected } from '../contexts/CountySelected';
+import { APICovidByCountyRequest } from '../contexts/APICovidByCountyRequest';
 
 const customStyles = {
   control: (provided) => ({
@@ -25,9 +26,21 @@ const customStyles = {
   }),
 };
 
-const Map = (props) => {
+const Map = () => {
+  const { setSelectedCountyName } = useContext(CountySelected);
+  const { allData } = useContext(APICovidByCountyRequest);
+  const allDataDep = allData
+    .filter((item) => item.code.includes('DEP'))
+    .map((item) => {
+      return {
+        ...item,
+        code: item.code.split('-')[1], // string format because of corsica
+      };
+    });
+
+  // API data are passed in props but not used
   const [windowWidth, setWindowWidth] = useState(window.innerWidth);
-  const [allData, setData] = useState([]);
+  // const [allData, setData] = useState([]);
   const [colorSelection, setColorSelection] = useState({
     value: '',
     label: '- Aucun code couleur -',
@@ -40,48 +53,23 @@ const Map = (props) => {
     { value: 'hosp', label: 'Personnes hospitalisées' },
   ];
 
-  // this API request is redundant and should be moved in another component, probably in a context
-  useEffect(() => {
-    const dayMinus1 = moment().subtract(1, 'days').format('YYYY-MM-DD'); // last available data
-    const { CancelToken } = axios;
-    const source = CancelToken.source();
-    axios
-      .get(
-        `https://coronavirusapi-france.now.sh/AllDataByDate?date=${dayMinus1}`,
-        {
-          cancelToken: source.token,
-        }
-      )
-      .then((response) => response.data)
-      .then((data) => {
-        setData(() =>
-          // keeping only counties, not regions and not the whole country
-          data.allFranceDataByDate.filter((item) => item.code.includes('DEP'))
-        );
-      })
-      .catch((err) => {
-        if (axios.isCancel(err)) {
-          console.log('Request canceled', err.message);
-        }
-      });
-    return function cleanup() {
-      source.cancel('Operation canceled by the user.');
-    };
-  }, []);
-
   // updating the value of customFrance, the data used to draw the map, depending on the choice made by the user in the select list
   useEffect(() => {
-    if (allData.length > 0 && colorSelection.value !== '') {
+    if (allDataDep.length > 0 && colorSelection.value !== '') {
       let selection = '';
+      let multiplier;
       switch (colorSelection.value) {
         case 'rea':
           selection = 'reanimation';
-          break;
-        case 'hosp':
-          selection = 'hospitalises';
+          multiplier = 1000000;
           break;
         case 'dead':
           selection = 'deces';
+          multiplier = 100000;
+          break;
+        case 'hosp':
+          selection = 'hospitalises';
+          multiplier = 100000;
           break;
         default:
           console.log(
@@ -92,23 +80,27 @@ const Map = (props) => {
         ...France,
         label: 'Custom map label',
         locations: France.locations.map((location) => {
-          const nb = allData.find(
-            (item) => item.code.split('-')[1] === location.id
-          )[selection];
+          const nb = allDataDep.find((item) => item.code === location.id)[
+            selection
+          ];
+          const { pop } = countyListPop.find(
+            (item) => item.code.toString() === location.id
+          );
+          const ratio = Math.round((nb / pop) * multiplier);
 
-          if (nb > 250) {
+          if (ratio > 100) {
             return {
               ...location,
               name: `${location.name}-red`,
             };
           }
-          if (nb > 80) {
+          if (ratio > 50) {
             return {
               ...location,
               name: `${location.name}-orange`,
             };
           }
-          if (nb > 30) {
+          if (ratio > 25) {
             return {
               ...location,
               name: `${location.name}-yellow`,
@@ -120,10 +112,10 @@ const Map = (props) => {
           };
         }),
       });
-    } else if (allData.length > 0 && colorSelection.value === '') {
+    } else if (allDataDep.length > 0 && colorSelection.value === '') {
       setCustomFrance(France);
     }
-  }, [allData, colorSelection]);
+  }, [allDataDep, colorSelection]);
 
   const handleResize = () => {
     setWindowWidth(window.innerWidth);
@@ -134,32 +126,36 @@ const Map = (props) => {
   }, []);
 
   const handleClick = (event) => {
-    props.onSelectCounty(event.target.id);
+    const { id } = event.target; // this extra step is necessary because the name in location is used, and modified, for color mapping | it cannot be passed straight away as an argument
+    const name = allDataDep.find((item) => item.code === id).nom;
+    setSelectedCountyName(name);
   };
 
   return (
-    <div className="map none">
-      {windowWidth < 600 ? (
-        <TransformWrapper>
-          <TransformComponent>
-            <SVGMap map={customFrance} onLocationClick={handleClick} />
-          </TransformComponent>
-        </TransformWrapper>
-      ) : (
-        <SVGMap map={customFrance} onLocationClick={handleClick} />
-      )}
-      <div>
-        <Select
-          options={options}
-          getOptionLabel={(option) => option.label}
-          onChange={(newValue) => setColorSelection(newValue)}
-          styles={customStyles}
-          noOptionsMessage={() => 'Aucun département trouvé'}
-          value={colorSelection}
-        />
+    <>
+      <div className="map none">
+        {windowWidth < 600 ? (
+          <TransformWrapper>
+            <TransformComponent>
+              <SVGMap map={customFrance} onLocationClick={handleClick} />
+            </TransformComponent>
+          </TransformWrapper>
+        ) : (
+          <SVGMap map={customFrance} onLocationClick={handleClick} />
+        )}
+        <div>
+          <Select
+            options={options}
+            getOptionLabel={(option) => option.label}
+            onChange={(newValue) => setColorSelection(newValue)}
+            styles={customStyles}
+            noOptionsMessage={() => 'Aucun département trouvé'}
+            value={colorSelection}
+          />
+        </div>
+        <p>Sélectionner les données à afficher par code couleur</p>
       </div>
-      <p>Sélectionner les données à afficher par code couleur</p>
-    </div>
+    </>
   );
 };
 
