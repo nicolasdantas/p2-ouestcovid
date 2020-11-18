0eb11c24a9bfec296caff208ed8f8acc35986b1a
nicolasdantas <nicolas.dantas@gmail.com>
2020-11-18 11:29:50 +0100

    geolocation is working

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,9 +1,30 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import Select from 'react-select';
+import Geocode from 'react-geocode';
 import countyList from './datas/countyList.json'; // data from https://geo.api.gouv.fr/departements
 import { CountySelected } from '../contexts/CountySelected';
 
+Geocode.setLanguage('fr');
+Geocode.setRegion('fr');
+Geocode.setApiKey('AIzaSyD9QyjcXrlW8JUHmsDjH2n2ttF-bPLyYjc')
+
 const SearchBar = () => {
+  useEffect(() => {
+    Geocode.fromLatLng('48.8583701', '2.2922926').then(
+      (response) => {
+        const address = response.results[0].formatted_address;
+        console.log(address);
+      },
+      (error) => {
+        console.error(error);
+      }
+    );
+    navigator.geolocation.getCurrentPosition((position) => {
+      console.log('Latitude is :', position.coords.latitude);
+      console.log('Longitude is :', position.coords.longitude);
+    });
+  }, []);
+
   const { selectedCountyName, setSelectedCountyName } = useContext(
     CountySelected
   );
