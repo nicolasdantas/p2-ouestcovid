aff1e291cc82327fd0f28692aa32fa57f2524490
nicolasdantas <nicolas.dantas@gmail.com>
2020-11-26 10:09:23 +0100

    modal added telling  user that form has benn sent

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import './style/ContactForm.scss';
 import axios from 'axios';
+import ContactConfirmation from './ContactConfirmation';
 
 const ContactForm = () => {
   const [name, setName] = useState('');
@@ -9,6 +10,7 @@ const ContactForm = () => {
   const [nameError, setNameError] = useState('');
   const [emailError, setEmailError] = useState('');
   const [messageError, setMessageError] = useState('');
+  const [modalShow, setModalShow] = useState(false);
 
   const handleNameChange = (event) => {
     setName(event.target.value);
@@ -40,20 +42,17 @@ const ContactForm = () => {
 
   const handleSubmit = (event) => {
     event.preventDefault();
+    setModalShow(true);
     const datas = {
       name,
       message,
       email,
     };
     console.log(datas);
-    axios
-      .post(
-        `https://new-app-form.herokuapp.com/contact?apiKey=${window.apiKey}`,
-        datas
-      )
-      .then(() => {
-        alert('okay great');
-      });
+    axios.post(
+      `https://new-app-form.herokuapp.com/contact?apiKey=${window.apiKey}`,
+      datas
+    );
     const isValid = validation();
     if (isValid) {
       // clear form
@@ -68,6 +67,10 @@ const ContactForm = () => {
 
   return (
     <div className="form-wrapper">
+      <ContactConfirmation
+        show={modalShow}
+        onHide={() => setModalShow(false)}
+      />
       <form id="contact" onSubmit={(event) => handleSubmit(event)}>
         <h2> Faites-nous part de vos remarques</h2>
         <div>
